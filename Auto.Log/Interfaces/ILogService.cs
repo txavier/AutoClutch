83d0cf286300cd2138733e7dc4c757f71b886dd8
txavier <TheoX@BPSLFK0007>
2016-09-13 10:20:57 -0400

    - Added missing Auto.Log files.

---
+++ Auto.Log/Interfaces/ILogService.cs
@@ -0,0 +1,34 @@
+﻿using AutoClutch.Auto.Core.Interfaces;
+using AutoClutch.Auto.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClutch.Auto.Log.Interfaces
+{
+    public interface ILogService : IService<userActionLog>
+    {
+        void Info(Exception ex);
+
+        IEnumerable<userActionLog> GetLatestUserActionLogs(int take = 10);
+
+        Task<userActionLog> InfoAsync(string typeName, string typeFullName, int recordId, EventType eventType, string message, string entityName, string loggedInUserName, string toString = null);
+
+        userActionLog Info(string typeName, string typeFullName, int recordId, EventType eventType, string message, string entityName, string loggedInUserName);
+    }
+
+    public interface ILogService<TEntity> : ILogService, IService<userActionLog>
+    {
+        new IEnumerable<userActionLog> GetLatestUserActionLogs(int take = 10);
+
+        Task<userActionLog> InfoAsync(TEntity entity, int recordId, EventType eventType, string message = null, string entityName = null, string loggedInUserName = null, bool useToString = false);
+
+        userActionLog Info(TEntity entity, int recordId, EventType eventType, string message = null, string entityName = null, string loggedInUserName = null);
+
+        Task<userActionLog> InfoAsync(string message);
+
+        userActionLog Info(string message);
+    }
+}
